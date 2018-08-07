d4e4775e599dd00f7d3ca7ebd1c9899c367494c0
Halil Bozan <halilbozan9@gmail.com>
2018-08-07 14:15:01 +0300

    json marka and year changed.

--- models/model.go
+++ models/model.go
@@ -5,6 +5,6 @@ import "gopkg.in/mgo.v2/bson"
 type Cars struct {
 	ID    bson.ObjectId `bson:"_id" json:"id"`
 	model string        `bson:"model_name" json:"model_name"`
-	year  string        `bson:"year" json:"year"`
-	marka string        `bson:"marka" json:"marka"`
+	year  string        `bson:"year_date" json:"year"`
+	marka string        `bson:"marka_name" json:"marka"`
 }
